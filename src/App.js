30f018a784bd0e4ac9089fb3a0d1253e7ffb82ab
kennymaccormak <volodymyr.chygura@gmail.com>
2018-09-05 22:49:51 +0300

    new branch articles; removed old files; paste new mock data;

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
-import {ContactsList} from './components/contacts-list';
 
 class App extends Component {
   render() {
-    return <ContactsList/> ;
+    return (
+      <div className="container">
+
+      </div>
+    );
   }
 }
 
