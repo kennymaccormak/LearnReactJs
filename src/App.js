1c533be6b74d72175c894018ae5ade0c3337fbbc
Volodymyr Chygura <volodymyr.chygura@gmail.com>
2018-09-14 17:50:33 +0300

    added react-select

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
 import ArticleList from './components/Article/ArticleList';
 import UserForm from './components/UserForm/UserForm';
-import {articles} from './mock/fixtures';
+import { articles } from './mock/fixtures';
+import Select from 'react-select';
 
 class App extends Component {
+  state = {
+    selection: null
+  };
+
+  changeSelection = (selection) => {
+    this.setState({
+      selection
+    });
+  };
 
   render() {
+    const options = articles.map(article => ({
+      label: article.title,
+      value: article.id
+    }));
     return (
       <div className="container">
         <UserForm/>
+        <Select options={options}
+                className="app-select"
+                value={this.state.selection}
+                isMulti
+                onChange={this.changeSelection}/>
         <ArticleList articles={articles}/>
       </div>
     );
