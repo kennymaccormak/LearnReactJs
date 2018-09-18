8a236ceca9c20748819d52b85d9f184e560838da
Volodymyr Chygura <volodymyr.chygura@gmail.com>
2018-09-18 18:01:30 +0300

    added datepicker

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
 import Select from 'react-select';
 
+import DayPicker, { DateUtils } from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
+import './App.css';
+
 import ArticleList from './components/Article/ArticleList';
 import UserForm from './components/UserForm/UserForm';
 import { articles } from './mock/fixtures';
 
 class App extends Component {
   state = {
-    selection: null
+    selection: null,
+    from: new Date(),
+    to: new Date()
   };
 
-  changeSelection = (selection) => {
+  changeSelection = selection => {
     this.setState({
       selection
     });
+  };
+
+  handleDayClick = day => {
+    const range = DateUtils.addDayToRange(day, this.state);
+    this.setState(range);
   };
 
   render() {
@@ -21,15 +32,30 @@ class App extends Component {
       label: article.title,
       value: article.id
     }));
+
+    const { from, to } = this.state;
+    const modifiers = { start: from, end: to };
+    const dayRange = (from && to) ? <p>from: {from.toLocaleDateString()}, to: {to.toLocaleDateString()}</p> : <p>Select date range</p>;
+
     return (
       <div className="container">
-        <UserForm/>
-        <Select options={options}
-                className="app-select"
-                value={this.state.selection}
-                isMulti
-                onChange={this.changeSelection}/>
-        <ArticleList articles={articles}/>
+        <DayPicker
+          className="range-picker"
+          numberOfMonths={1}
+          selectedDays={[from, { from, to }]}
+          modifiers={modifiers}
+          onDayClick={this.handleDayClick}
+        />
+        {dayRange}
+        <UserForm />
+        <Select
+          options={options}
+          className="app-select"
+          value={this.state.selection}
+          isMulti
+          onChange={this.changeSelection}
+        />
+        <ArticleList articles={articles} />
       </div>
     );
   }
