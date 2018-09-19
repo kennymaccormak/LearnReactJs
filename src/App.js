b3c8256598317e892093e582cd95267e48b2b2f8
Volodymyr Chygura <volodymyr.chygura@gmail.com>
2018-09-19 17:00:12 +0300

    fix app comp

--- src/App.js
+++ src/App.js
@@ -1,60 +1,16 @@
 import React, { Component } from 'react';
-import Select from 'react-select';
-
-import DayPicker, { DateUtils } from 'react-day-picker';
-import 'react-day-picker/lib/style.css';
-import './App.css';
 
 import ArticleList from './components/Article/ArticleList';
 import UserForm from './components/UserForm/UserForm';
 import { articles } from './mock/fixtures';
 
+import './App.css';
+
 class App extends Component {
-  state = {
-    selection: null,
-    from: new Date(),
-    to: new Date()
-  };
-
-  changeSelection = selection => {
-    this.setState({
-      selection
-    });
-  };
-
-  handleDayClick = day => {
-    const range = DateUtils.addDayToRange(day, this.state);
-    this.setState(range);
-  };
-
   render() {
-    const options = articles.map(article => ({
-      label: article.title,
-      value: article.id
-    }));
-
-    const { from, to } = this.state;
-    const modifiers = { start: from, end: to };
-    const dayRange = (from && to) ? <p>from: {from.toLocaleDateString()}, to: {to.toLocaleDateString()}</p> : <p>Select date range</p>;
-
     return (
       <div className="container">
-        <DayPicker
-          className="range-picker"
-          numberOfMonths={1}
-          selectedDays={[from, { from, to }]}
-          modifiers={modifiers}
-          onDayClick={this.handleDayClick}
-        />
-        {dayRange}
-        <UserForm />
-        <Select
-          options={options}
-          className="app-select"
-          value={this.state.selection}
-          isMulti
-          onChange={this.changeSelection}
-        />
+        <UserForm articles={articles} />
         <ArticleList articles={articles} />
       </div>
     );
--- src/components/UserForm/UserForm.js
+++ src/components/UserForm/UserForm.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 
+import Select from 'react-select';
+import DayPicker, { DateUtils } from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
+
 export default class UserForm extends Component {
   state = {
-    username: ''
+    username: '',
+    selection: null,
+    from: new Date(),
+    to: new Date()
   };
 
-  handleUserChange = (ev) => {
+  changeSelection = selection => {
+    this.setState({
+      selection
+    });
+  };
+
+  handleDayClick = day => {
+    const range = DateUtils.addDayToRange(day, this.state);
+    this.setState(range);
+  };
+
+  handleUserChange = ev => {
     if (ev.target.value.length > 10) return;
     this.setState({
       username: ev.target.value
@@ -13,9 +31,44 @@ export default class UserForm extends Component {
   };
 
   render() {
+    const options = this.props.articles.map(article => ({
+      label: article.title,
+      value: article.id
+    }));
+    const { from, to } = this.state;
+    const modifiers = { start: from, end: to };
+    const dayRange =
+      from && to ? (
+        <p>
+          from: {from.toLocaleDateString()}, to: {to.toLocaleDateString()}
+        </p>
+      ) : (
+        <p>Select date range</p>
+      );
     return (
       <div>
-        Name: <input type="text" className="form-control" value={this.state.username} onChange={this.handleUserChange} />
+        <DayPicker
+          className="range-picker"
+          numberOfMonths={1}
+          selectedDays={[from, { from, to }]}
+          modifiers={modifiers}
+          onDayClick={this.handleDayClick}
+        />
+        {dayRange}
+        Name:{' '}
+        <input
+          type="text"
+          className="form-control"
+          value={this.state.username}
+          onChange={this.handleUserChange}
+        />
+        <Select
+          options={options}
+          className="app-select"
+          value={this.state.selection}
+          isMulti
+          onChange={this.changeSelection}
+        />
       </div>
     );
   }
