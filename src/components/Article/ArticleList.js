4b393505a2b75506e0fadf77168b415c3af2607f
Volodymyr Chygura <volodymyr.chygura@gmail.com>
2018-09-24 16:14:08 +0300

    added simple selector

--- src/components/Article/ArticleList.js
+++ src/components/Article/ArticleList.js
@@ -4,6 +4,7 @@ import { connect } from "react-redux";
 
 import Article from "./Article";
 import accordion from "../../decorators/accordion";
+import {filtratedArticlesSelector} from '../../selectors';
 
 class ArticleList extends Component {
   render() {
@@ -22,21 +23,9 @@ class ArticleList extends Component {
   }
 }
 
-export default connect(({ filters, articles }) => {
-  const {
-    selected,
-    dateRange: { from, to }
-  } = filters;
-
-  const filteredArticles = articles.filter(article => {
-    const published = Date.parse(article.date);
-    return (
-      (!selected.length || selected.includes(article.id)) &&
-      (!from || !to || (published > from && published < to))
-    );
-  });
-
+export default connect((state) => {
+  
   return {
-    articles: filteredArticles
+    articles: filtratedArticlesSelector(state)
   }
 })(accordion(ArticleList));
